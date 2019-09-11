d65830b6aeb99d529dd15bf85a424e7679387534
alexey zotov <zotov.alexei.1@yandex.ru>
2019-09-11 20:36:53 +0300

    trying to cache

--- alice_app.py
+++ alice_app.py
@@ -10,7 +10,37 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-session_storage = {}  # храним данные о сессиях
+import pylibmc
+import os
+
+servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
+user = os.environ.get('MEMCACHIER_USERNAME', '')
+passw = os.environ.get('MEMCACHIER_PASSWORD', '')
+
+mc = pylibmc.Client(servers, binary=True,
+                    username=user, password=passw,
+                    behaviors={
+                      # Faster IO
+                      'tcp_nodelay': True,
+
+                      # Keep connection alive
+                      'tcp_keepalive': True,
+
+                      # Timeout for set/get requests
+                      'connect_timeout': 2000, # ms
+                      'send_timeout': 750 * 1000, # us
+                      'receive_timeout': 750 * 1000, # us
+                      '_poll_timeout': 2000, # ms
+
+                      # Better failover
+                      'ketama': True,
+                      'remove_failed': 1,
+                      'retry_timeout': 2,
+                      'dead_timeout': 30,
+                    })
+
+
+mc.set("session_storage", {})
 
 
 @app.route("/", methods=['POST'])
@@ -22,9 +52,13 @@ def main():
 
     user_id = alice_request.user_id
 
-    alice_response, session_storage[user_id] = handle_dialog(
-        alice_request, alice_response, session_storage.get(user_id)
+    alice_response, storage = handle_dialog(
+        alice_request, alice_response, mc.get("session_storage").get(user_id)
     )
+
+    sessions = mc.get("session_storage")
+    sessions[user_id] = storage
+    mc.set("session_storage", sessions)
 
     logging.info('Response: {}'.format(alice_response))
 
